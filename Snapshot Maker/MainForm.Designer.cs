0f655c5407682964208d9bac533911573f7e8686
Mirek <mirekh1991@gmial.com>
2013-03-22 13:05:39 +0100

    dodano kod
    Bla Bla kod w c# sialala

---
+++ Snapshot Maker/MainForm.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Snapshot_Maker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.devicesCombo = new System.Windows.Forms.ComboBox();
+            this.videoResolutionsCombo = new System.Windows.Forms.ComboBox();
+            this.snapshotResolutionsCombo = new System.Windows.Forms.ComboBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.triggerButton = new System.Windows.Forms.Button();
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_hough = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button_progowanie = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // devicesCombo
+            // 
+            this.devicesCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.devicesCombo.FormattingEnabled = true;
+            this.devicesCombo.Location = new System.Drawing.Point(12, 15);
+            this.devicesCombo.Name = "devicesCombo";
+            this.devicesCombo.Size = new System.Drawing.Size(412, 21);
+            this.devicesCombo.TabIndex = 1;
+            this.devicesCombo.SelectedIndexChanged += new System.EventHandler(this.devicesCombo_SelectedIndexChanged);
+            // 
+            // videoResolutionsCombo
+            // 
+            this.videoResolutionsCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.videoResolutionsCombo.FormattingEnabled = true;
+            this.videoResolutionsCombo.Location = new System.Drawing.Point(12, 42);
+            this.videoResolutionsCombo.Name = "videoResolutionsCombo";
+            this.videoResolutionsCombo.Size = new System.Drawing.Size(222, 21);
+            this.videoResolutionsCombo.TabIndex = 3;
+            // 
+            // snapshotResolutionsCombo
+            // 
+            this.snapshotResolutionsCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.snapshotResolutionsCombo.FormattingEnabled = true;
+            this.snapshotResolutionsCombo.Location = new System.Drawing.Point(240, 45);
+            this.snapshotResolutionsCombo.Name = "snapshotResolutionsCombo";
+            this.snapshotResolutionsCombo.Size = new System.Drawing.Size(184, 21);
+            this.snapshotResolutionsCombo.TabIndex = 4;
+            this.toolTip.SetToolTip(this.snapshotResolutionsCombo, "Press shutter button on your camera to make snapshot");
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(430, 15);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 6;
+            this.connectButton.Text = "&Start";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Location = new System.Drawing.Point(430, 45);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(75, 23);
+            this.disconnectButton.TabIndex = 7;
+            this.disconnectButton.Text = "&Stop";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // toolTip
+            // 
+            this.toolTip.AutoPopDelay = 5000;
+            this.toolTip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.toolTip.InitialDelay = 100;
+            this.toolTip.ReshowDelay = 100;
+            // 
+            // triggerButton
+            // 
+            this.triggerButton.Location = new System.Drawing.Point(511, 15);
+            this.triggerButton.Name = "triggerButton";
+            this.triggerButton.Size = new System.Drawing.Size(150, 55);
+            this.triggerButton.TabIndex = 9;
+            this.triggerButton.Text = "&ZDJECIE";
+            this.triggerButton.UseVisualStyleBackColor = true;
+            this.triggerButton.Click += new System.EventHandler(this.triggerButton_Click);
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.videoSourcePlayer.ForeColor = System.Drawing.Color.DarkRed;
+            this.videoSourcePlayer.Location = new System.Drawing.Point(12, 75);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.Size = new System.Drawing.Size(322, 242);
+            this.videoSourcePlayer.TabIndex = 0;
+            this.videoSourcePlayer.VideoSource = null;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(341, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_hough
+            // 
+            this.button_hough.Location = new System.Drawing.Point(667, 15);
+            this.button_hough.Name = "button_hough";
+            this.button_hough.Size = new System.Drawing.Size(133, 23);
+            this.button_hough.TabIndex = 11;
+            this.button_hough.Text = "&Transformata Hougha";
+            this.button_hough.UseVisualStyleBackColor = true;
+            this.button_hough.Click += new System.EventHandler(this.button_hough_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(667, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(667, 75);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button_progowanie
+            // 
+            this.button_progowanie.Location = new System.Drawing.Point(905, 12);
+            this.button_progowanie.Name = "button_progowanie";
+            this.button_progowanie.Size = new System.Drawing.Size(129, 23);
+            this.button_progowanie.TabIndex = 14;
+            this.button_progowanie.Text = "Progowanie";
+            this.button_progowanie.UseVisualStyleBackColor = true;
+            this.button_progowanie.Click += new System.EventHandler(this.button_progowanie_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(905, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(806, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(93, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 332);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button_progowanie);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_hough);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.videoSourcePlayer);
+            this.Controls.Add(this.triggerButton);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.snapshotResolutionsCombo);
+            this.Controls.Add(this.videoResolutionsCombo);
+            this.Controls.Add(this.devicesCombo);
+            this.MinimumSize = new System.Drawing.Size(680, 370);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snapshot Maker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox devicesCombo;
+        private System.Windows.Forms.ComboBox videoResolutionsCombo;
+        private System.Windows.Forms.ComboBox snapshotResolutionsCombo;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.Button triggerButton;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_hough;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button_progowanie;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Snapshot Maker/MainForm.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Imaging.Textures;
+using AForge.Imaging.ComplexFilters;
+
+namespace Snapshot_Maker
+{
+    public partial class MainForm : Form
+    {
+        private FilterInfoCollection urzadzenia;
+        private VideoCaptureDevice kamera;
+        private VideoCapabilities[] parametry_video;
+        private VideoCapabilities[] parametry_zdjecia;
+
+        private SnapshotForm snapshotForm = null;
+
+        private Bitmap zdjecie;
+        private Bitmap zdjecie_binarne;
+
+
+        public MainForm( )
+        {
+            InitializeComponent( );
+        }
+
+        // Main form is loaded
+        private void MainForm_Load( object sender, EventArgs e )
+        {
+            // enumerate video devices
+            urzadzenia = new FilterInfoCollection( FilterCategory.VideoInputDevice );
+
+            if ( urzadzenia.Count != 0 )
+            {
+                // add all devices to combo
+                foreach ( FilterInfo device in urzadzenia )
+                {
+                    devicesCombo.Items.Add( device.Name );
+                }
+            }
+            else
+            {
+                devicesCombo.Items.Add( "No DirectShow devices found" );
+            }
+
+            devicesCombo.SelectedIndex = 0;
+
+            EnableConnectionControls( false ); // dostep do zmiany jakosci photo/video
+            devicesCombo.Enabled = true;
+            connectButton.Enabled = true;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = pictureBox1.InitialImage;
+
+        }
+
+        // Closing the main form
+        private void MainForm_FormClosing( object sender, FormClosingEventArgs e )
+        {
+            Disconnect( );
+        }
+
+        // Enable/disable connection related controls
+        private void EnableConnectionControls( bool enable )
+        {
+            devicesCombo.Enabled = enable;
+            videoResolutionsCombo.Enabled = enable;
+            snapshotResolutionsCombo.Enabled = enable;
+            connectButton.Enabled = enable;
+            disconnectButton.Enabled = !enable;
+            triggerButton.Enabled = ( !enable ) && ( parametry_zdjecia.Length != 0 );
+        }
+
+        // New video device is selected
+        private void devicesCombo_SelectedIndexChanged( object sender, EventArgs e )
+        {
+            if ( urzadzenia.Count != 0 )
+            {
+                kamera = new VideoCaptureDevice( urzadzenia[devicesCombo.SelectedIndex].MonikerString );
+                EnumeratedSupportedFrameSizes( kamera );
+            }
+        }
+
+        // Collect supported video and snapshot sizes
+        private void EnumeratedSupportedFrameSizes( VideoCaptureDevice kamera )
+        {
+            this.Cursor = Cursors.WaitCursor;
+
+            videoResolutionsCombo.Items.Clear( );
+            snapshotResolutionsCombo.Items.Clear( );
+
+            try
+            {
+                parametry_video = kamera.VideoCapabilities;
+                parametry_zdjecia = kamera.SnapshotCapabilities;
+                kamera.DesiredFrameRate = 50;
+
+                foreach ( VideoCapabilities capabilty in parametry_video )
+                {
+                    if ( !videoResolutionsCombo.Items.Contains( capabilty.FrameSize ) )
+                    {
+                        videoResolutionsCombo.Items.Add( capabilty.FrameSize );
+                    }
+                }
+
+                foreach ( VideoCapabilities capabilty in parametry_zdjecia )
+                {
+                    if ( !snapshotResolutionsCombo.Items.Contains( capabilty.FrameSize ) )
+                    {
+                        snapshotResolutionsCombo.Items.Add( capabilty.FrameSize );
+                    }
+                }
+
+                if ( parametry_video.Length == 0 )
+                {
+                    videoResolutionsCombo.Items.Add( "Not supported" );
+                }
+                if ( parametry_zdjecia.Length == 0 )
+                {
+                    snapshotResolutionsCombo.Items.Add( "Not supported" );
+                }
+
+                videoResolutionsCombo.SelectedIndex = 0;
+                snapshotResolutionsCombo.SelectedIndex = 0;
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        // On "Connect" button clicked
+        private void connectButton_Click( object sender, EventArgs e )
+        {
+            if ( kamera != null )
+            {
+                if ( ( parametry_video != null ) && ( parametry_video.Length != 0 ) )
+                {
+                    kamera.DesiredFrameSize = (Size) videoResolutionsCombo.SelectedItem;
+                }
+
+                if ( ( parametry_zdjecia != null ) && ( parametry_zdjecia.Length != 0 ) )
+                {
+                    kamera.ProvideSnapshots = true;
+                    kamera.DesiredSnapshotSize = (Size) snapshotResolutionsCombo.SelectedItem;
+                    //kamera.SnapshotFrame += new NewFrameEventHandler( kamera_SnapshotFrame );
+                }
+
+                EnableConnectionControls( false );
+
+                videoSourcePlayer.VideoSource = kamera;
+                videoSourcePlayer.Start( );
+            }
+        }
+
+        // On "Disconnect" button clicked
+        private void disconnectButton_Click( object sender, EventArgs e )
+        {
+            Disconnect( );
+        }
+
+        // Disconnect from video device
+        private void Disconnect( )
+        {
+            if ( videoSourcePlayer.VideoSource != null )
+            {
+                // stop video device
+                videoSourcePlayer.SignalToStop( );
+                videoSourcePlayer.WaitForStop( );
+                videoSourcePlayer.VideoSource = null;
+
+                if ( kamera.ProvideSnapshots )
+                {
+                    kamera.SnapshotFrame -= new NewFrameEventHandler( kamera_SnapshotFrame );
+                }
+
+                EnableConnectionControls( true );
+            }
+        }
+
+        // Simulate snapshot trigger
+        private void triggerButton_Click( object sender, EventArgs e )
+        {
+            if ( ( kamera != null ) && ( kamera.ProvideSnapshots ) )
+            {
+                kamera.SimulateTrigger( );
+                kamera.SnapshotFrame += new NewFrameEventHandler(kamera_SnapshotFrame);
+            }
+        }
+
+        // New snapshot frame is available
+        private void kamera_SnapshotFrame( object sender, NewFrameEventArgs eventArgs )
+        {
+            // zrob zdjecie :
+            Bitmap old = (Bitmap)pictureBox1.Image;
+            zdjecie = (Bitmap)eventArgs.Frame.Clone();
+            //pictureBox1 = new PictureBox();
+            pictureBox1.Image = zdjecie;
+
+            // greyscale :
+            Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
+            Bitmap temp = (Bitmap)pictureBox1.Image;
+            zdjecie = (Bitmap)filter.Apply(temp);
+            
+            pictureBox1.Image = (Bitmap)zdjecie;
+            pictureBox1.Update();
+            // czyszczenie pamieci starego zdjecia
+            /*
+            if (old != null)
+            {
+                old.Dispose();
+            }
+             */
+        }
+
+        private void ShowSnapshot( Bitmap snapshot )
+        {
+            if ( InvokeRequired )
+            {
+                Invoke( new Action<Bitmap>( ShowSnapshot ), snapshot );
+            }
+            else
+            {
+                if ( snapshotForm == null )
+                {
+                    snapshotForm = new SnapshotForm( );
+                    snapshotForm.FormClosed += new FormClosedEventHandler( snapshotForm_FormClosed );
+                    snapshotForm.Show( );
+                }
+
+                snapshotForm.SetImage( snapshot );
+            }
+        }
+
+        private void snapshotForm_FormClosed( object sender, FormClosedEventArgs e )
+        {
+            snapshotForm = null;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void button_hough_Click(object sender, EventArgs e)
+        {
+            Bitmap temp = (Bitmap)zdjecie_binarne;
+            HoughCircleTransformation transformata_hougha = new HoughCircleTransformation(Convert.ToInt32(textBox1.Text));
+            transformata_hougha.ProcessImage(temp);
+            
+            textBox3.Text = transformata_hougha.CirclesCount.ToString();
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            
+
+            HoughCircle[] circles = transformata_hougha.GetCirclesByRelativeIntensity(0.2);
+            foreach (HoughCircle circle in circles)
+            {
+                string s = string.Format("X = {0}, Y = {1}, I = {2} ({3})", circle.X, circle.Y, circle.Intensity, circle.RelativeIntensity);
+                System.Diagnostics.Debug.WriteLine(s);
+                
+            }
+
+            temp = transformata_hougha.ToBitmap();
+            pictureBox2.Image = (Bitmap)temp;
+        }
+
+        private void button_progowanie_Click(object sender, EventArgs e)
+        {
+            Threshold progowanie = new Threshold();
+            Bitmap obrazek = (Bitmap)zdjecie;
+            progowanie.ThresholdValue = Convert.ToInt32(textBox2.Text);
+            obrazek = progowanie.Apply(obrazek);
+            pictureBox1.Image = obrazek;
+            pictureBox1.Update();
+
+            zdjecie_binarne = obrazek;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int temp = 0;
+            textBox1.Text += Convert.ToString(temp++);
+        }
+    }
+}
